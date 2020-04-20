0ac04fe504432ef2db617978c1a4e8337c802fb4
ittisafur <ittisafur@gmail.com>
2020-04-21 01:35:38 +0600

    Initialization

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const cleancss = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+
+function style() {
+  return gulp
+    .src("src/sass/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleancss())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+}
+
+// The `build` function is exported so it is public and can be run with the `gulp` command.
+// It can also be used within the `series()` composition.
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./src",
+      index: "index.html",
+    },
+  });
+  gulp.watch("src/sass/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.default = watch;
